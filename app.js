e846ca1ee71e94fcd9fbaae14d149a134ed30ff5
Wali Shah <walishahllc@gmail.com>
2024-06-28 16:38:01 +0500

    Array

---
+++ app.js
@@ -0,0 +1,7 @@
+let str = "Idocoding";
+
+// console.log(str.replace('do', 'love')); //replace method
+
+// console.log(str.replace('do', 'love').repeat('2'));  //i used method chaining
+
+console.log(str.repeat('4'));  //repeat method
